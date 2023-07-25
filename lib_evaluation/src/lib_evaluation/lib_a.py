cc03b0fd4803d1d343cb5a7edd683277a034459e
Florian Engelhorn <you@example.com>
2023-07-25 22:26:25 +0200

    works with pandas

--- lib_evaluation/src/lib_evaluation/lib_a.py
+++ lib_evaluation/src/lib_evaluation/lib_a.py
@@ -5,7 +5,7 @@ def get_df():
     return pd.DataFrame(
         data={
             "timestamp": [1.0, 2.0, 3.0],
-            "lat": [8.0, 9.0],
-            "lon": [48.0, 49.0]
+            "lat": [8.0, 9.0, 10.0],
+            "lon": [48.0, 49.0, 50.0]
         }
     )
